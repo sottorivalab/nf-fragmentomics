e0598a5192d19b14d2909ef59c318dbb826e3ce1
Davide Rambaldi <davide.rambaldi@fht.org>
2024-04-22 16:29:51 +0200

    sample_sheet generator

--- scripts/samplesheet_generator.py
+++ scripts/samplesheet_generator.py
@@ -4,9 +4,17 @@ import sys
 import csv
 import argparse
 import logging
+import re
+from pathlib import Path
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Generate samplesheet.csv")
+    parser.add_argument(
+        dest="bams",
+        help="Bam files",
+        nargs='+'
+    )
+
     parser.add_argument(
             "-v",
             "--verbose",
@@ -15,6 +23,7 @@ def parse_args():
             action="store_const",
             const=logging.INFO,
             )
+    
     parser.add_argument(
             "-vv",
             "--very-verbose",
@@ -23,6 +32,7 @@ def parse_args():
             action="store_const",
             const=logging.DEBUG,
             )
+
     args = parser.parse_args()
     return args
 
@@ -40,7 +50,18 @@ def setup_logging(loglevel):
 def main():
     args = parse_args()
     setup_logging(args.loglevel)
-    print(args)
+    bams = [Path(b) for b in args.bams]
+    print("caseid,sampleid,timepoint,bam,bai")
+    for bam in bams:
+        case_name = bam.parents[3].name
+        sample_name = bam.parents[2].name
+        timepoint = re.sub(".*[0-9]+_","",sample_name)
+        bai_file = Path(str(bam).replace("bam","bai"))
+        if not bai_file.is_file():
+            print("ERROR!")
+            return 1
+
+        print(f"{case_name},{sample_name},{timepoint},{bam.absolute()},{bai_file.absolute()}")
 
 if __name__ == "__main__":
     sys.exit(main())
